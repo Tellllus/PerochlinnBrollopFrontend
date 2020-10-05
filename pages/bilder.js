bf56eb3239fe11c42288cf330a9d5a37a5eb2a60
Noah Mayerhofer <noah.mayerhofer@hotmail.com>
2020-10-05 18:02:47 +0200

    bilder created

---
+++ pages/bilder.js
@@ -0,0 +1,62 @@
+import Head from 'next/head'
+import React from 'react';
+import Navigation from '../components/navigation'
+import Card from '../components/card'
+import HeaderImage from '../components/header-image'
+import styles from './css/bilder.module.css'
+import gql from 'graphql-tag';
+import Query  from '../components/query';
+import RichText  from '../components/rich-text';
+
+export default function Home() {
+
+  const query = gql`
+  {
+    bilder {
+      header
+      page_content
+    }
+    navigation {
+      links{
+        ... on ComponentNavigationNavigationElement {
+        link_text
+        link_url
+      	}
+      }
+    }
+  }
+`
+  return (
+   
+  
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
+        <title>Bröllop</title>
+      </Head>
+
+      <Query query={query} id={null}>
+      {({ data: { boende: {header, page_content}, navigation: {links} }}) => {
+        return (
+          <div>
+             <Navigation elements={links}/>
+            <main className={styles.wrapper}>
+          
+              <Card className={styles.card}>
+                <h1>
+                  {header}
+                </h1>
+                <RichText >{page_content}</RichText>
+              </Card>
+            </main>
+          </div>
+          
+        );
+      }}
+      </Query>
+    
+    </div> 
+  )
+}       
