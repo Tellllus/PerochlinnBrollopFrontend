d86907be56cece1e6fbb24e2a0fb2fa5fca7f025
Noah Mayerhofer <noahmay@users.noreply.github.com>
2021-12-02 20:30:26 +0100

    update graphql

--- pages/brollopsdagen.js
+++ pages/brollopsdagen.js
@@ -5,108 +5,104 @@ import Card from '../components/card'
 import SmallImage from '../components/small-image'
 import styles from './css/brollopsdagen.module.css'
 import gql from 'graphql-tag';
-import Query  from '../components/query';
-import RichText  from '../components/rich-text';
+import Query from '../components/query';
+import RichText from '../components/rich-text';
 import InfoBox from '../components/info-box';
 
 export default function Home() {
 
   const query = gql`
   {
-    brollopsdagen {
-      header
-      header_text
-      infobox{
-        ... on ComponentBrollopBrollopInfoBox {
+    brollop {
+     data {
+      attributes {
+        header
+        header_text
+        hashtag_img {data {attributes {url}}}
+        hashtag_text
+        hashtag_header
+        InfoBox {
           header
-          image{
-            url
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
           }
           text
         }
       }
-      hashtag_img{
-        url
-      }
-      hashtag_header
-      hashtag_text
     }
-    navigation {
-      links{
-        ... on ComponentNavigationNavigationElement {
-        link_text
-        link_url
-      	}
-      }
-    }
+  }
   }
 `
 
- const chunk = (array, size) => {
-  return array.reduce((chunks, item, i) => {
-    if (i % size === 0) {
-      chunks.push([item]);
-    } else {  
-      chunks[chunks.length - 1].push(item);
-    }
-    return chunks;
-  }, []);
-}
+  const chunk = (array, size) => {
+    return array.reduce((chunks, item, i) => {
+      if (i % size === 0) {
+        chunks.push([item]);
+      } else {
+        chunks[chunks.length - 1].push(item);
+      }
+      return chunks;
+    }, []);
+  }
   return (
-   
-  
+
+
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
         <title>Bröllop</title>
       </Head>
 
       <Query query={query} id={null}>
-      {({ data: {brollopsdagen: {header, header_text, infobox, hashtag_img, hashtag_header, hashtag_text}, navigation: {links} }}) => {
-        
-        {
-          console.log(chunk(infobox,2))
-        }
-        return (
-          <div>
-             <Navigation elements={links}/>
-            <main className={styles.wrapper}>
-              
-            <Card className={styles.card}>
-                <h1>
-                  {header}
-                </h1>
+        {({ data: { brollopsdagen: { data: { attributes: { header, header_text, InfoBox, hashtag_img, hashtag_header, hashtag_text } } }/*, navigation: { links } */ } }) => {
 
-                <p>{header_text}</p>
+          {
+            console.log(chunk(infobox, 2))
+          }
+          return (
+            <div>
+              <Navigation elements={links} />
+              <main className={styles.wrapper}>
 
-                <div className={styles.infoboxes}>
-                  {chunk(infobox,2).map( (element) => {
+                <Card className={styles.card}>
+                  <h1>
+                    {header}
+                  </h1>
+
+                  <p>{header_text}</p>
+
+                  <div className={styles.infoboxes}>
+                    {chunk(infobox, 2).map((element) => {
                       let first = element[0]
                       let second = element[1]
-                      return(
+                      return (
                         <div className={styles.row}>
-                          <InfoBox className={styles.left} url={process.env.API_URL + first.image.url} header ={first.header} text={first.text}/> 
-                          {second ? <InfoBox className={styles.right} url={process.env.API_URL + second.image.url} header ={second.header} text={second.text}/> : ""} 
+                          <InfoBox className={styles.left} url={process.env.API_URL + first.image.url} header={first.header} text={first.text} />
+                          {second ? <InfoBox className={styles.right} url={process.env.API_URL + second.image.url} header={second.header} text={second.text} /> : ""}
                         </div>
-                      ) 
-                  })}
-                </div>
+                      )
+                    })}
+                  </div>
 
-              <div className={styles.hashtag}>
-                <img src={process.env.API_URL + hashtag_img.url}/> 
-                <h3>{hashtag_header}</h3>
-                <RichText>{hashtag_text}</RichText>
-                </div>
-              </Card>
-            </main>
-          </div>
-          
-        );
-      }}
+                  <div className={styles.hashtag}>
+                    <img src={process.env.API_URL + hashtag_img.url} />
+                    <h3>{hashtag_header}</h3>
+                    <RichText>{hashtag_text}</RichText>
+                  </div>
+                </Card>
+              </main>
+            </div>
+
+          );
+        }}
       </Query>
-    
-    </div> 
+
+    </div>
   )
-}       
+}
--- pages/stjarnor.js
+++ pages/stjarnor.js
@@ -5,82 +5,83 @@ import Card from '../components/card'
 import HeaderImage from '../components/header-image'
 import styles from './css/stjarnor.module.css'
 import gql from 'graphql-tag';
-import Query  from '../components/query';
-import RichText  from '../components/rich-text';
+import Query from '../components/query';
+import RichText from '../components/rich-text';
 
 
 export default function Home() {
 
   const query = gql`{
-   
-    stjarnor{
-         data {
-         attributes {
-           header
-           header_image {
-            data {
+    stjarnor {
+      data {
+        attributes {
+          header
+          page_content
+          header_images {
+            data{
               attributes {
                 url
               }
             }
           }
-          page_content
-         }
         }
-       
-   }}`
+      }
+    }
+  }`
 
-    /*
-    navigation {
-      links{
-        ... on ComponentNavigationNavigationElement {
-        link_text
-        link_url
-      	}
+  /*
+  navigation {
+    links{
+      ... on ComponentNavigationNavigationElement {
+      link_text
+      link_url
       }
     }
   }
- `*/
+}
+`*/
   return (
-   
-  
+
+
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700&display=swap" rel="stylesheet" />
         <title>Bröllop</title>
       </Head>
 
       <Query query={query} id={null}>
-      {({ data: { stjarnor: {data:{attributes: {header, header_image:{data:{attributes: {url}}}, text_on_header_image, page_content} }} }}) => {
+        {({ data: { stjarnor: { data: { attributes: { header, header_images, page_content } } } } }) => {
 
-        return (
+          return (
             <div>
-            {/* <Navigation elements={links}/>*/}
-           <main className={styles.wrapper}>
-             
-           <div className={styles.img}>
-             <HeaderImage className={styles.headerImage} url={process.env.API_URL + url} text={text_on_header_image}/>
-             
-             </div>
-         
-             <Card className={styles.card}>
-               <h1>
-                 {header}
-               </h1>
-               <RichText >{page_content}</RichText>
-             </Card>
-           </main>
-         </div>
-          
-        );
-      }}
+              {/* <Navigation elements={links}/>*/}
+              <main className={styles.wrapper}>
+
+                <div className={styles.imageContainer}>
+                  {
+                    header_images.data.map(image => (
+                      <img src={process.env.API_URL + image.attributes.url} />
+                    ))
+                  }
+                </div>
+
+                <Card className={styles.card}>
+                  <h1>
+                    {header}
+                  </h1>
+                  <RichText >{page_content}</RichText>
+                </Card>
+              </main>
+            </div>
+
+          );
+        }}
       </Query>
-    
-    </div> 
+
+    </div>
   )
-}       
+}
 
 
-            
